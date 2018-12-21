02313037e658524a056bcdf3dc5ea8d365600281
tonicont <tonitrix@gmail.com>
2018-12-21 14:39:40 +0100

    Users can create groups

---
+++ src/app/groups.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupsService {
+
+  constructor() { }
+
+  getGroupsByUser(userId: string): any {
+      const database = firebase.database();
+      return database.ref('users/' + userId + '/groups').once('value');
+  }
+
+  createGroup(groupName: string): void {
+    const database = firebase.database();
+    const userId = firebase.auth().currentUser.uid;
+    const newGroupKey = database.ref().child('groups').push().key;
+    const users = {};
+    users[userId] = true;
+    const newGroup = {
+      groupName: groupName,
+      users: users
+    };
+    const updates = {};
+    updates['/groups/' + newGroupKey] = newGroup;
+    updates['/users/' + userId + /groups/ + newGroupKey] = true;
+    database.ref().update(updates);
+  }
+}
--- src/app/groups/groups.component.ts
+++ src/app/groups/groups.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GroupsService } from '../groups.service';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-groups',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groups.component.scss']
 })
 export class GroupsComponent implements OnInit {
+  groups = [];
 
-  constructor() { }
+  constructor(private groupsService: GroupsService) { }
 
   ngOnInit() {
+    this.getGroupsFromDatabase(firebase.auth().currentUser.uid);
+  }
+
+  getGroupsFromDatabase(userId: string): void {
+    const groups = [];
+    this.groupsService.getGroupsByUser(userId).then(function (data) {
+      data.forEach(function(childSnapshot) {
+        groups.push(childSnapshot.key);
+      });
+    });
+    this.groups = groups;
+  }
+
+  createNewGroup(): void {
+    this.groupsService.createGroup('Grupo de testing');
   }
 
 }
